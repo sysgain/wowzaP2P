1ff0240d5468acd6c3ad6f9b71cad4a3f248bede
vcherukuri <vcherukuri@sysgain.com>
2016-09-21 16:06:59 +0530

    Update delete.sh

--- wowza/delete.sh
+++ wowza/delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for var in `sh pollsa.sh | tr -s " " "#" | cut -f2 -d "#" | sed '1,4d' | sed '$d'`
 do
-result="azure storage blob delete videos "$var" /usr/local/WowzaStreamingEngine/content/"$var
+result="azure storage blob delete videos "$var
 echo $result
 done
