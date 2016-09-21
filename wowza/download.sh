34c38be6c97a355bef18b8779cb1d5d407ba9c16
vcherukuri <vcherukuri@sysgain.com>
2016-09-21 16:01:19 +0530

    Update download.sh

--- wowza/download.sh
+++ wowza/download.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for var in `sh pollsa.sh | tr -s " " "#" | cut -f2 -d "#" | sed '1,4d' | sed '$d'`
 do
 result="azure storage blob download videos "$var" /usr/local/WowzaStreamingEngine/content/"$var
