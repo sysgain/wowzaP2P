e5dda633963dca110fe112e79c3220b3adcb4ea7
nvtuluva <nvtuluva@sysgain.com>
2016-09-23 23:33:55 +0530

    Update configwowza.sh

--- wowza/configwowza.sh
+++ wowza/configwowza.sh
@@ -27,7 +27,8 @@ touch /tmp/videos
 chmod 777 /tmp/videos
 echo "export AZURE_STORAGE_ACCOUNT="$2 >>pollsa.sh
 echo "export AZURE_STORAGE_ACCESS_KEY="$3 >>pollsa.sh
-echo "/usr/local/bin/azure storage blob list videos | sed 's/    /;/g' | awk -F';' '{print $(2)}' | sed 's/B.*//' | sed '1,4d' | sed '$(d)' > /tmp/videos" >> pollsa.sh
+wget https://raw.githubusercontent.com/sysgain/wowzaP2P/master/wowza/scripts/command
+cat command >>pollsa.sh
 sudo chmod 777 pollsa.sh
 
 echo "#!/bin/bash" >>download1.sh
